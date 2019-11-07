53b2d30bd9d01f549329fb3dc8fb37fb618d7e03
gugu1201 <55218487+gugu1201@users.noreply.github.com>
2019-11-07 10:45:58 +0800

    人工智慧_陳學文_B1040426

---
+++ UnityProject/Assets/Ball.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    Rigidbody r3d;
+    [Header("速度")]
+    [Range(1,100)]
+    public float speed = 1;
+    [Header("跳躍")]
+    [Range(1, 100)]
+    public float jump = 1;
+    [Header("有無接觸地面")]
+    public bool isGround;
+    
+
+    // Use this for initialization
+    void Start()
+    {
+        r3d = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Walk();
+        Jump();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isGround = true;
+        Debug.Log("touch" + collision.gameObject);
+    }
+
+    private void Walk()
+    {
+        r3d.AddForce(new Vector3(speed * Input.GetAxis("Horizontal"), 0, 0));
+        r3d.AddForce(new Vector3(0, 0, speed * Input.GetAxis("Vertical")));
+
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGround == true)
+        {
+            isGround = false;
+            r3d.AddForce(new Vector3(0, jump, 0));
+        }
+
+    }
+}
---
+++ UnityProject/Assets/NPC.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    [Header("對話")]
+    public string Say = "走開!!!!!!!!";
+    public float SaySpeed = 1f;
+    [Header("任務")]
+    public bool Complete = false;
+    public int BateryFinish = 10;
+    public int Batery = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
